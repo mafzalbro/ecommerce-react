3552a9e376856a97febaff0389276a68d3033f25
mafzalbro <mafzalbro@gmail.com>
2024-11-23 16:54:13 +0500

    push

--- src/components/layout/navbar/SellerMenu.jsx
+++ src/components/layout/navbar/SellerMenu.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
-import { AiOutlineDashboard, AiOutlineAppstore } from "react-icons/ai";
+import {
+  AiOutlineDashboard,
+  AiOutlineAppstore,
+  AiOutlineDownCircle,
+} from "react-icons/ai";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -7,6 +11,8 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
 } from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"; // Import the avatar components
+import useAuth from "../../../hooks/AuthProvider";
 
 const sellerMenuItems = [
   {
@@ -22,28 +28,43 @@ const sellerMenuItems = [
 ];
 
 // eslint-disable-next-line react/prop-types
-const SellerMenu = ({ className }) => (
-  <DropdownMenu>
-    <DropdownMenuTrigger className="text-lg font-medium">
-      Seller Dashboard
-    </DropdownMenuTrigger>
-    <DropdownMenuContent className="w-[200px] p-2">
-      <DropdownMenuLabel>Menu</DropdownMenuLabel>
-      <div className={`space-y-2 ${className}`}>
-        {sellerMenuItems.map((item, index) => (
-          <DropdownMenuItem key={index}>
-            <Link
-              to={item.link}
-              className="flex items-center"
-            >
-              {item.icon}
-              <span className="ml-2">{item.label}</span>
-            </Link>
-          </DropdownMenuItem>
-        ))}
-      </div>
-    </DropdownMenuContent>
-  </DropdownMenu>
-);
+const SellerMenu = ({ className }) => {
+  const { user } = useAuth();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="text-lg font-medium flex items-center space-x-2">
+        <Avatar className="w-8 h-8">
+          <AvatarImage
+            src={`https://api.dicebear.com/6.x/initials/svg?seed=${user?.name}`}
+            alt={user?.name}
+          />
+          <AvatarFallback>
+            {user?.name
+              ?.split(" ")
+              .map((n) => n[0])
+              .join("")}
+          </AvatarFallback>
+        </Avatar>
+        <small className="text-xs hidden sm:block">
+          Hey, Seller
+        </small>
+        <AiOutlineDownCircle className="ml-1 w-4 h-4 text-gray-500" />{" "}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-[200px] p-2">
+        <DropdownMenuLabel>Menu</DropdownMenuLabel>
+        <div className={`space-y-2 ${className}`}>
+          {sellerMenuItems.map((item, index) => (
+            <DropdownMenuItem key={index}>
+              <Link to={item.link} className="flex items-center">
+                {item.icon}
+                <span className="ml-2">{item.label}</span>
+              </Link>
+            </DropdownMenuItem>
+          ))}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 export default SellerMenu;
